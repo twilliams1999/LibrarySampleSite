b2600a1e562aeb8026c8550c7be72e19487bc2df
Taylor Williams <taylor.williams1999@outlook.com>
2024-08-27 18:37:18 -0400

    Worked on correctly sizing hero image for screen size.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:size-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="items-center justify-between font-mono text-sm lg:flex">
+        <div className="bottom-0 flex h-48 w-full items-end justify-center lg:static lg:bg-none">
             <Image
               src="/logo.png"
               alt="Library Logo"
-              className="dark:invert"
               width={240}
               height={112}
               priority
@@ -16,15 +15,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/hero.svg"
-          alt="Image of the front of the fictional Chbosky Public Library with the name printed on the front"
-          width={1920}
-          height={448}
-          priority
-        />
+      <div className="bottom-0 bg-center place-items-center w-full h-[21rem] lg:static" style={{backgroundImage:"url(/hero.svg)",backgroundSize:"95rem"}}>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
